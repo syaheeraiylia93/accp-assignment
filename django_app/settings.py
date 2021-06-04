483afc2f5d2af202d6e7171ce6d1bdfdf205ddb6
Nur Syaheera Iylia Shallehudin <syaheera@advbin.net>
2021-06-04 21:18:49 +0000

    Initial Django App

--- django_app/settings.py
+++ django_app/settings.py
@@ -76,20 +76,16 @@ WSGI_APPLICATION = 'django_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if DEVELOPMENT_MODE is True:
-        DATABASES = {
-                        "default": {
-                                        "ENGINE": "django.db.backends.sqlite3",
-                                                    "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-                                                            }
-                            }
-    elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
-            if os.getenv("DATABASE_URL", None) is None:
-                        raise Exception("DATABASE_URL environment variable not defined")
-                        DATABASES = {
-                                        "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
-                                            }
-
+DATABASES = {
+             'default': {
+                            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+                            'NAME': 'defaultdb',
+                            'USER' : 'doadmin',
+                            'PASSWORD' : 'qwtq2v3cksrvb556',
+                            'HOST' : 'accp-posgres-do-user-9271829-0.b.db.ondigitalocean.com'
+                            'PORT': 25060
+                         }
+             }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
